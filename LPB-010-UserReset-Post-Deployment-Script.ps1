6b3c82f420fe0955e55475a5fd3192abf72cf92d
Hugh <5219141+machug@users.noreply.github.com>
2023-06-26 13:29:21 +1000

    Add files via upload

---
+++ LPB-010-UserReset-Post-Deployment-Script.ps1
@@ -0,0 +1,30 @@
+﻿$MIGuid = "<GUID>"
+$SubscriptionId = "<SUBID>"
+$ResourceGroupName = "<RG>"
+
+Connect-AzureAD
+
+$MI = Get-AzureADServicePrincipal -ObjectId $MIGuid
+$GraphAppId = "00000003-0000-0000-c000-000000000000"
+$roleName = "Password Administrator"
+$SentinelRoleName = "Microsoft Sentinel Responder"
+$PermissionName = "User.ReadWrite.All" 
+
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+
+#Grant Azure AD Password Administrator role
+$role = Get-AzureADDirectoryRole | Where {$_.displayName -eq $roleName}
+if ($role -eq $null) {
+$roleTemplate = Get-AzureADDirectoryRoleTemplate | Where {$_.displayName -eq $roleName}
+Enable-AzureADDirectoryRole -RoleTemplateId $roleTemplate.ObjectId
+$role = Get-AzureADDirectoryRole | Where {$_.displayName -eq $roleName}
+}
+Add-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -RefObjectId $MI.ObjectID
+
+#Grant Resource Group Sentinel Responder role
+New-AzRoleAssignment -ObjectId $MIGuid -RoleDefinitionName $SentinelRoleName -Scope /subscriptions/$SubscriptionId/resourcegroups/$ResourceGroupName
+
+#Grant Azure AD Permissions
+$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
+-ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
---
+++ LPB-016-Restrict-MDEUrl-MI.ps1
@@ -0,0 +1,47 @@
+#Requires -Modules Microsoft.Graph
+# Install the module. (You need admin on the machine.)
+# Install-Module Microsoft.Graph
+
+# Set Static Variables
+# Your tenant
+$TenantID="WORKSPACEID"
+# The LogicApp name you created the Managed Identiy in
+$ServicePrincipalAppDisplayName ="LPB-016-Restrict-MDEUrl-MI"
+
+# Define dynamic variables
+$ServicePrincipalFilter = "displayName eq '$($ServicePrincipalAppDisplayName)'" 
+$GraphAPIAppName = "WindowsDefenderATP"
+$ApiServicePrincipalFilter = "displayName eq '$($GraphAPIAppName)'"
+
+# Scopes needed for the managed identity (Add other scopes if needed)
+$Scopes = @(
+    "Ti.ReadWrite.All"
+    "Ti.ReadWrite"
+)
+
+# Connect to MG Graph - scopes must be consented the first time you run this. 
+# Connect with Global Administrator
+Connect-MgGraph -Scopes "Application.Read.All","AppRoleAssignment.ReadWrite.All"  -TenantId $TenantID -UseDeviceAuthentication
+
+# Get the service principal for your managed identity.
+$ServicePrincipal = Get-MgServicePrincipal -Filter $ServicePrincipalFilter
+
+# Get the service principal for Microsoft Graph. 
+# Result should be the AppId 
+$ApiServicePrincipal = Get-MgServicePrincipal -Filter "$ApiServicePrincipalFilter"
+
+# Apply permissions
+Foreach ($Scope in $Scopes) {
+    Write-Host "`nGetting App Role '$Scope'"
+    $AppRole = $ApiServicePrincipal.AppRoles | Where-Object {$_.Value -eq $Scope -and $_.AllowedMemberTypes -contains "Application"}
+    if ($null -eq $AppRole) { Write-Error "Could not find the specified App Role on the Api Service Principal"; continue; }
+    if ($AppRole -is [array]) { Write-Error "Multiple App Roles found that match the request"; continue; }
+    Write-Host "Found App Role, Id '$($AppRole.Id)'"
+
+    $ExistingRoleAssignment = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $ServicePrincipal.Id | Where-Object { $_.AppRoleId -eq $AppRole.Id }
+    if ($null -eq $existingRoleAssignment) {
+        New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $ServicePrincipal.Id -PrincipalId $ServicePrincipal.Id -ResourceId $ApiServicePrincipal.Id -AppRoleId $AppRole.Id
+    } else {
+        Write-Host "App Role has already been assigned, skipping"
+    }
+} 
